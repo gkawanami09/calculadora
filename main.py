6c1249ca72e1e2cbc1040c40ea81cec92bdfb897
gkawanami09 <guilherme.kawanami@gmail.com>
2025-04-25 15:22:26 -0300

    Update - acionamento

--- main.py
+++ main.py
@@ -12,21 +12,28 @@ def calcular():
         num2 = float(request.form['num2'])
         operador = str(request.form['operador'])
         resultado = 0
+        acionar = 0
         if operador == '+':
             resultado = num1+num2
+            acionar = 1
         elif operador == '-':
             resultado = num1-num2
+            acionar = 1
         elif operador == '*':
             resultado = num1*num2
+            acionar = 1
         elif operador == '/':
             if num2 == 0:
                 resultado = 'impossível dividir por 0'
+                acionar = 1
             else:
                 resultado = num1/num2
+                acionar = 1
         else:
             resultado = 'operador inexistente'
+            acionar = 1
         
-        return render_template('index.html',resultado=resultado)
+        return render_template('index.html',resultado=resultado, acionar=acionar)
     except ValueError as e:
         return render_template('erro.html', mensagem=f"Entrada inválida: {e}")
     finally:
